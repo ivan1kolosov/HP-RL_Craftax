5f449310e06ec21d59f1ce5aed6e170da2620eb4
ivan1kolosov <kolosovivan222@gmail.com>
2025-07-11 15:50:58 +0300

    misprint fix

--- Agents/tacticAgent.py
+++ Agents/tacticAgent.py
@@ -1,8 +1,9 @@
 class Scenario:
     def __init__(self):
         self.values = None
+        self.task = map() #what is the best way to implement this?
 
-    def get_maps(self, state) -> tuple: #(torch.Tensor, torsch.Tensor)
+    def get_maps(self, state) -> tuple: #(torch.Tensor, torch.Tensor)
         return NotImplementedError
     
     def is_action(self):
@@ -10,13 +11,13 @@ class Scenario:
     
     def get_action(self):
         return NotImplementedError
+    
+    def get_reward(self, state, next_state) -> float:
+        return NotImplementedError
 
 class TacticAgent:
     def __init__(self):
         pass
 
     def get_scen(self, state):
-        return NotImplementedError
-    
-    def get_reward(self, state, next_state) -> float:
         return NotImplementedError
